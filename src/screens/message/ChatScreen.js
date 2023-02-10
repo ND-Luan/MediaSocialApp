7e6c55aec362f464c90abea011d6770c0f0238f4
ngodinhvu123 <ngodinhvuluan@gmail.com>
2023-02-10 10:22:28 +0700

    add rngiftchat

--- src/screens/message/ChatScreen.js
+++ src/screens/message/ChatScreen.js
@@ -1,30 +1,39 @@
-import {EmojiIcon} from '@components/Icon';
-import TextInput from '@components/TextInput';
-import View from '@components/View';
-import COLOR from '@constants/COLOR';
-import ChatCard from './components/chat/ChatCard';
-import Button from '@components/Button';
-import Background from '@components/Background';
+import React, {useState, useCallback, useEffect} from 'react';
+import {GiftedChat} from 'react-native-gifted-chat';
+
 const ChatScreen = () => {
+  const [messages, setMessages] = useState([]);
+
+  useEffect(() => {
+    setMessages([
+      {
+        _id: 1,
+        text: 'Hello developer',
+        createdAt: new Date(),
+        user: {
+          _id: 2,
+          name: 'React Native',
+          avatar: 'https://placeimg.com/140/140/any',
+        },
+      },
+    ]);
+  }, []);
+
+  const onSend = useCallback((messages = []) => {
+    setMessages(previousMessages =>
+      GiftedChat.append(previousMessages, messages),
+    );
+  }, []);
+
   return (
-    <Background
-      backgroundColor={COLOR.lightPink}
-      padding={0}
-      justifyContent="space-between">
-      <View padding={0}>
-        <ChatCard />
-        <ChatCard />
-        <ChatCard />
-        <ChatCard />
-        <ChatCard />
-        <ChatCard />
-        <ChatCard />
-        <ChatCard />
-        <ChatCard />
-        <ChatCard />
-      </View>
-      <TextInput />
-    </Background>
+    <GiftedChat
+      messages={messages}
+      onSend={messages => onSend(messages)}
+      user={{
+        _id: 1,
+      }}
+    />
   );
 };
+
 export default ChatScreen;
